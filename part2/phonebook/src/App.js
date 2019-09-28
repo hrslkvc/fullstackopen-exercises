b335ae08e2fbf81b8063d5b0ea1ad308d08ac66c
haris <hareluuu@gmail.com>
2019-09-28 10:37:50 +0200

    Phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import Filter from "./components/Filter";
 import Form from "./components/Form";
@@ -43,6 +44,12 @@ const App = () => {
         setNewName("");
         setNewNumber("");
     };
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:3001/persons")
+            .then(response => setPersons(response.data));
+    }, []);
 
     return (
         <div>
